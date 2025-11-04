af48245c0f11820aa5517ff29f432ceeed2d1b13
Gustavo Martinez Rollano <erwin@apoloenergies.es>
2025-11-04 07:44:17 -0400

    refactor: cors

--- src/app/services/ApiManager/ApiManager.ts
+++ src/app/services/ApiManager/ApiManager.ts
@@ -4,7 +4,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 export const ApiManager = axios.create({
   baseURL: process.env.NEXT_PUBLIC_ENVIRONMENT_PROD,
   responseType: "json",
-  withCredentials: true,
+  // withCredentials: true,
 });
 ;
 export function setAuthToken(token: string): void {
